046a406895fc123199d7a93cdbca07e555f5610a
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-04-11 15:51:46 -0600

    Update logo

---
+++ resources/views/vendor/base/back/layouts/widget/central.blade.php
@@ -0,0 +1,12 @@
+@extends('base::back.layouts.empty')
+
+@section('content')
+<div class="p-3 min-h-full flex flex-colum flex-wrap content-center justify-center drop-shadow-2xl">
+    <div class="lg:w-2/6 flex flex-col bg-neutral-content rounded-3xl p-10">
+        <div class="flex flex-row justify-center">
+            <img src="{{ asset('img/logo.svg') }}" width="40%"  />
+        </div>
+        @yield('central')
+    </div>
+</div>
+@endsection
