02f555458a4a2fa41eb25d61ec03b6157c50aab4
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-04-13 08:57:29 -0600

    Implement pusher

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Events\NewOrder;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        NewOrder::dispatch('anime');
+
         return view('back.dashboard.index', [
         ]);
     }
