55ef9bda99e7c0fe2ff43f0ca344b4f4a73aec62
Camilo Rodriguez <camilo@sdkconsultoria.com>
2023-04-14 22:22:59 -0600

    Delete unused code

--- database/migrations/2023_04_14_130245_create_orders_table.php
+++ database/migrations/2023_04_14_130245_create_orders_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('sales', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->commonFields();
+            $table->foreignId('client_id');
         });
     }
 
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sales');
+        Schema::dropIfExists('orders');
     }
 };
