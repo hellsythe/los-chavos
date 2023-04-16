ef56d980c4611d7151a0396c509b0943fea2928f
Camilo Rodriguez <camilo@sdkconsultoria.com>
2023-04-15 21:50:38 -0600

    Format price on ticket

--- resources/back/js/components/Sale/Ticket.vue
+++ resources/back/js/components/Sale/Ticket.vue
@@ -16,20 +16,28 @@
                     <template v-for="(service, index) in selectedServices">
                         <tr>
                             <td style="font-size: 12px; border: 1px solid; padding: 2px">{{ service.subservice_id.name }} </td>
-                            <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: end;">${{ service.price?.toFixed(2) * garmentData.amount }}</td>
+                            <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: end;">{{ formatter(service.price * garmentData.amount) }}</td>
                         </tr>
                         <tr v-if="service.subservice_id.id == 4 && garmentData.amount <= 6">
                             <td style="font-size: 12px; border: 1px solid; padding: 2px"> Costo Por Diseño nuevo</td>
-                            <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: end;">${{ service.price_new?.toFixed(2) }} </td>
+                            <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: end;">{{ formatter(service.price_new) }} </td>
                         </tr>
                         <tr v-if="service.subservice_id.id == 3  && garmentData.amount <= 6">
                             <td style="font-size: 12px; border: 1px solid; padding: 2px"> Costo Por Modificar diseño </td>
-                            <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: end;">${{ service.price_update?.toFixed(2) }}</td>
+                            <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: end;">{{ formatter(service.price_update) }}</td>
                         </tr>
                     </template>
                     <tr>
                         <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: right;"><strong>Total</strong></td>
-                        <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: right;"><strong>${{ payment.total }}</strong></td>
+                        <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: right;"><strong>{{ formatter(payment.total) }}</strong></td>
+                    </tr>
+                    <tr>
+                        <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: right;"><strong>Anticipo</strong></td>
+                        <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: right;"><strong>{{ formatter(payment.advance) }}</strong></td>
+                    </tr>
+                    <tr>
+                        <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: right;"><strong>Resta</strong></td>
+                        <td style="font-size: 12px; border: 1px solid; padding: 2px; text-align: right;"><strong>{{ formatter(payment.total - payment.advance) }}</strong></td>
                     </tr>
                 </table>
             </div>
@@ -37,6 +45,7 @@
 </template>
 
 <script>
+import money from './../formater';
 
 export default {
     name: "Ticket",
@@ -73,6 +82,9 @@ export default {
             var mm = String(today.getMonth() + 1).padStart(2, '0');
             var yyyy = today.getFullYear();
             this.date = dd + '-' + mm + '-' + yyyy;
+        },
+        formatter(amount){
+            return  money.format(amount);
         }
     },
 };
