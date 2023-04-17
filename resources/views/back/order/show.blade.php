5652c6292c948c7ef15dfa8d467cabb0428fb13a
Camilo Rodriguez <camilo@sdkconsultoria.com>
2023-04-16 21:51:35 -0600

    Added details

--- resources/views/back/order/show.blade.php
+++ resources/views/back/order/show.blade.php
@@ -4,9 +4,24 @@
 
 @section('content')
     <?= Base::breadcrumb([$model->getRoute('index') => $model->getTranslation('plural'), $model->getTranslation('showed')]) ?>
-    <div id=app>
-
+    <div class="p-4 bg-base-200 mb-5 shadow rounded-lg">
+        <div class="flex justify-between">
+            <h1 class="font-bold">Información del cliente</h1>
+        </div>
+        <div class="flex">
+            <div class="form-control w-full mb-2 mr-2">
+                <label class="label"><span class="label-text">Nombre del cliente</span></label>
+                <input type="text" class="input input-bordered w-full" value="{{$model->client->name}}" readonly>
+            </div>
+            <div class="form-control w-full mb-2 mr-2">
+                <label class="label"><span class="label-text">Teléfono del cliente</span></label>
+                <input type="number" class="input input-bordered w-full" value="{{$model->client->phone}}" readonly>
+            </div>
+            <div class="form-control w-full mb-2">
+                <label class="label"><span class="label-text">Correo del cliente</span></label>
+                <input type="email" class="input input-bordered w-full" value="{{$model->client->email}}" readonly>
+            </div>
+        </div>
     </div>
-
     @yield('model')
 @endsection
