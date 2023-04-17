456523c774a99fa9ebf9b179c1c0a0112df06ecc
Camilo Rodriguez <camilo@sdkconsultoria.com>
2023-04-16 21:34:36 -0600

    Redirect to scan

--- resources/views/back/order/show.blade.php
+++ resources/views/back/order/show.blade.php
@@ -6,45 +6,6 @@
     <?= Base::breadcrumb([$model->getRoute('index') => $model->getTranslation('plural'), $model->getTranslation('showed')]) ?>
     <div id=app>
 
-    <div class="mb-2 flex flex-row">
-        <a type="button" href="{{ $model->getRoute('update', $model->getKeyId()) }}" class="btn btn-primary">
-            {!! $model->getTranslation('edit') !!} </a>
-            <delete-model
-                :translations='{!! json_encode($model->getFullTranslations()) !!}'
-                :routes='{{json_encode($model->getIndexRoutes())}}'
-                :model_id='{{$model->id}}'
-            />
-        </div>
-    </div>
-
-    <div class="flex flex-col">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        @php
-                            $fields = $model->getFields();
-                        @endphp
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            <tr>
-                                <th class="text-gray-700 p-2"> {{ $fields['name']['label'] }} </th>
-                                <td class="p-2"> {{ $model->name }} </td>
-                            </tr>
-                            <tr>
-                                <th class="text-gray-700 p-2"> {{ $fields['minutes']['label'] }} </th>
-                                <td class="p-2"> {{ $model->minutes }} </td>
-                            </tr>
-                            <tr>
-                                <th class="text-gray-700 p-2"> {{ $fields['media']['label'] }} </th>
-                                <td class="p-2">
-                                    <a href="{{ $model->media }}" class="link link-info" target="_blank"> Ver </a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
     </div>
 
     @yield('model')
