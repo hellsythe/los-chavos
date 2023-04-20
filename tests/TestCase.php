23cae77b753847428a4d0759228bc70f20e2f0d5
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-04-20 12:52:48 -0600

    Added test create user

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,36 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+
+    protected function makeUser(array $params = []): User
+    {
+        $params['factory'] = $params['factory'] ?? [];
+        $user = $this->findIfUserExist($params['factory']);
+
+        if (! $user) {
+            $user = User::factory($params['factory'])->create();
+        }
+
+        if ($params['permission'] ?? false) {
+            $user->givePermissionTo($params['permission']);
+        }
+
+        if ($params['roles'] ?? false) {
+            $user->assignRole($params['roles']);
+        }
+
+        return $user;
+    }
+
+    protected function findIfUserExist(array $factory)
+    {
+        return User::where('email', $factory['email'] ?? '')->first();
+    }
 }
