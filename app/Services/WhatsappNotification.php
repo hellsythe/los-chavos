4be23f72a8a44dd1d09fe0d85754bc901c182189
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-04-25 11:53:50 -0600

    Add components

--- app/Services/WhatsappNotification.php
+++ app/Services/WhatsappNotification.php
@@ -13,19 +13,42 @@ class WhatsappNotification
     {
         foreach ($this->getAllAdminis() as $user) {
             if ($user->phone) {
-                $this->send($user->phone, 'notificar_solicitud');
+                $this->send(
+                    $user->phone,
+                    'solicitud_autorizacion_orden',
+                    [
+                        [
+                            "type" => "body",
+                            "parameters" => [
+                                [
+                                    "type" => "text",
+                                    "text" => $order->id
+                                ],
+                            ]
+                        ],
+                        [
+                            "type" => "button",
+                            "sub_type" => "url",
+                            "index" => "0",
+                            "parameters" => [
+                                [
+                                    "type" => "text",
+                                    "text" => $order->id
+                                ]
+                            ]
+                        ]
+                    ]
+                );
             }
         }
     }
 
     public function sendApprovedNotification()
     {
-
     }
 
     public function sendNewOrder()
     {
-
     }
 
     protected function send($number, $template, $components = [])
@@ -37,49 +60,15 @@ class WhatsappNotification
             "template" => [
                 "name" => $template,
                 "language" => ["code" => "es_MX"],
+                'components' => $components
             ],
         ]);
     }
 
     protected function getAllAdminis()
     {
-        return User::whereHas("roles", function($q){ $q->where("name", "super-admin"); })->get();
+        return User::whereHas("roles", function ($q) {
+            $q->where("name", "super-admin");
+        })->get();
     }
-
-    // $request = Http::withToken(config('app.whatsapp_token'))->post('https://graph.facebook.com/v16.0/' . config('app.whatsapp_phone_id') . '/messages', [
-    //     "messaging_product" => "whatsapp",
-    //     "to" => "522213428198",
-    //     "type" => "template",
-    //     "template" => [
-    //         "name" => "pedido_aprobado",
-    //         "language" => ["code" => "es_MX"],
-    //         "components" => [
-    //             [
-    //                 "type" => "body",
-    //                 "parameters" => [
-    //                     [
-    //                         "type" => "text",
-    //                         "text" => "8675"
-    //                     ],
-    //                     [
-    //                         "type" => "text",
-    //                         "text" => "Camilo rodriguez"
-    //                     ],
-    //                 ]
-    //             ],
-    //             [
-    //                 "type" => "button",
-    //                 "sub_type" => "url",
-    //                 "index" => "0",
-    //                 "parameters" => [
-    //                     [
-    //                         "type" => "text",
-    //                         "text" => "443"
-    //                     ]
-    //                 ]
-    //             ]
-    //         ]
-    //     ],
-    // ]);
-    // dump($request->body());
 }
