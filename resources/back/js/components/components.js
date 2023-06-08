0c1935f1e5182f5a5064efb13459d8b19cd4b72d
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-06-08 12:00:02 -0600

    optimize

--- resources/back/js/components/components.js
+++ resources/back/js/components/components.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import SalePoint from "./SalePoint/Index.vue";
-import showOrder from "./SaleDetails/ShowOrder.vue";
 import PaymentDetail from "./SaleDetails/PaymentDetail.vue";
-import PrintButton from "./Sale/PrintButton.vue";
 import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
 import ContextMenu from '@imengyu/vue3-context-menu'
 
@@ -11,11 +9,8 @@ let element = document.getElementById('sale')
 if (element !== null) {
     const app = createApp({}).use(ContextMenu);
 
-    app.component('showOrderComponent', showOrder)
     app.component('PaymentDetailComponent', PaymentDetail)
-    app.component('PrintButton', PrintButton)
     app.component('SalePoint', SalePoint)
-
 
     app.mount('#sale');
 
