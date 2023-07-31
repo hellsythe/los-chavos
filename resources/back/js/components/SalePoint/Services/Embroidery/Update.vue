15e7a33acdbd863395b8e3406b526ee086d25f54
Camilo Rodriguez <camilo@sdkconsultoria.com>
2023-07-31 14:56:41 -0600

    Fix

--- resources/back/js/components/SalePoint/Services/Embroidery/Update.vue
+++ resources/back/js/components/SalePoint/Services/Embroidery/Update.vue
@@ -4,13 +4,13 @@
             <label for="" class="label"><span class="label-text">Costo por ponchado modificado</span></label>
             <label class="input-group">
                 <span>$</span>
-                <input type="number" class="input input-bordered" v-model="service.detail.design.price" />
+                <input type="number" class="input input-bordered" v-model="service.detail.design.price" readonly />
             </label>
             <div class="text-red-500 text-xs font-semibold mt-1">{{ errors.detail.design.price }}</div>
         </div>
         <div class="form-control mb-2">
             <label for="" class="label"><span class="label-text">Puntadas</span></label>
-            <input type="number" class="input input-bordered" v-model="service.detail.design.minutes"/>
+            <input type="number" class="input input-bordered" v-model="service.detail.design.minutes" />
             <div class="text-red-500 text-xs font-semibold mt-1">{{ errors.detail.design.minutes }}</div>
         </div>
     </div>
@@ -32,6 +32,9 @@ export default {
     props: {
         service: JSON,
         errors: JSON,
+    },
+    mounted() {
+        this.service.detail.design.price = 45;
     },
     data() {
         return {
