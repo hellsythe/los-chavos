f1d6f200f3f2a64d9ef8a9efab3f1363c3aefcf7
Camilo Rodriguez <hellsythex@gmail.com>
2023-08-21 18:36:12 -0600

    fix

--- resources/views/back/order/index.blade.php
+++ resources/views/back/order/index.blade.php
@@ -9,7 +9,8 @@
     <?php
     $array = array_merge([], ['id', 'client.name', 'deadline', 'status']);
     $array_translation = array_merge($model->getFullTranslations(), ['client.name' => 'Cliente', 'client' => 'Cliente', 'deadline' => 'Fecha de entrega']);
-    $array_search = array_merge($model->getParseSearchFilters(), [
+    $array_search = array_merge([], [
+        ['field' => 'id'],
         ['field' => 'client'],
         ['field' => 'status', 'options' => $model->statusMapping(), 'type' => 'select']
     ]);
