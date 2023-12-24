0c1a4cf6b69e3bd7b570c342ba8da4565eeb09e7
Camilo Rodriguez <hellsythex@gmail.com>
2023-12-23 20:50:08 -0600

    update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call('app:load-statistics-embrodeiry')->daily();
+        $schedule->call('app:load-statistics-embrodeiry')->dailyAt('23:00');
     }
 
     /**
