fe6787ab0a611e85ab5a9706cf44b84d49f1d0b7
Camilo Rodriguez <camilo@sdkconsultoria.com>
2025-11-30 21:16:35 -0600

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (app()->environment('production')) {
-            \URL::forceScheme('https');
-        }
+        // if (app()->environment('production')) {
+        //     \URL::forceScheme('https');
+        // }
         Event::subscribe(UserEventSubscriber::class);
     }
 }
