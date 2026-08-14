ec019cc56fcb26a21fb7892912e72a96fb1f3e78
Camilo Rodriguez <camilo@sdkconsultoria.com>
2026-08-13 21:27:03 -0600

    fix

--- app/Http/Controllers/Admin/SchoolController.php
+++ app/Http/Controllers/Admin/SchoolController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\School;
+use Illuminate\Http\Request;
 use Sdkconsultoria\Core\Controllers\ResourceController;
 
 class SchoolController extends ResourceController
 {
     protected $model = \App\Models\School::class;
+
+    public function show(Request $request, $id)
+    {
+        $model = $this->model::findModel($id);
+        $model->isAuthorize('view');
+
+        return view('back.school.show', [
+            'model' => $model,
+        ]);
+    }
 }
---
+++ resources/views/back/school/show.blade.php
@@ -0,0 +1,42 @@
+@extends('base::back.layouts.app')
+
+@section('title', $model->getTranslation('showed'))
+
+@section('content')
+    <?= Base::breadcrumb([$model->getRoute('index') => $model->getTranslation('plural'), $model->getTranslation('showed')]) ?>
+
+    <div class="mb-2 flex flex-row">
+        <a type="button" href="{{ $model->getRoute('update', $model->getKeyId()) }}" class="btn btn-primary">
+            {!! $model->getTranslation('edit') !!}
+        </a>
+        <delete-model :translations='{!! json_encode($model->getFullTranslations()) !!}' :routes='{{ json_encode($model->getIndexRoutes()) }}'
+            :model_id='{{ $model->id }}' />
+    </div>
+
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach ($model->getFields() as $field)
+                                    <tr>
+                                        <th class="text-gray-700 p-2"> {{ $field['label'] }} </th>
+                                        <td class="p-2">
+                                            @if ($field['name'] === 'logo' && $model->logo)
+                                                <img src="{{ $model->logo }}" alt="Logo" class="max-h-48 rounded border" />
+                                            @else
+                                                {{ $model->{$field['name']} }}
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
