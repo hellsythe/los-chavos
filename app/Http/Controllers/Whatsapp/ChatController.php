6cb12169bda9434c9cd129b593712b900b2e78d6
Camilo Rodriguez <camilo@sdkconsultoria.com>
2026-08-14 19:44:07 -0600

    wip

--- app/Http/Controllers/Whatsapp/ChatController.php
+++ app/Http/Controllers/Whatsapp/ChatController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers\Whatsapp;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Sdkconsultoria\WhatsappCloudApi\Http\Controllers\ChatController as SdkChatController;
 
 class ChatController extends SdkChatController
 {
     protected function defaultOptions($models, Request $request)
     {
+        $lastMessageSubquery = DB::table('messages')
+            ->selectRaw('MAX(timestamp)')
+            ->whereColumn('chat_id', 'chats.id');
+
         return $models
-            ->orderBy('id', 'DESC');
+            ->orderByRaw('COALESCE((' . $lastMessageSubquery->toSql() . '), chats.updated_at, chats.created_at) DESC')
+            ->orderBy('chats.id', 'DESC');
     }
 }
